01b2b8362c1f25bfcea3b175d9da2f55f01f3698
changqiongli <changqiongli@hotmail.com>
2016-09-18 17:29:32 -0400

    Update and rename Login.java to CommonMethods.java

---
+++ src/Generic/CommonMethods.java
@@ -0,0 +1,36 @@
+package Generic;
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class CommonMethods{
+ 
+  public void static login( String username, String password) throws Exception{
+  driver.get(baseUrl);
+  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+  driver.findElement(By.id("username")).clear();
+  driver.findElement(By.id("username")).sendKeys("username");
+  driver.findElement(By.id("login")).clear();
+  driver.findElement(By.id("login")).sendKeys("password");
+  driver.findElement(By.cssSelector("input.form-button")).click();
+  
+  //driver.findElement(By.linkText("Log Out")).click();
+  //assertEquals("Admin Page",driver.getTitle());
+  }
+  
+   
+
+}
+
+ 
